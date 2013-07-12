2cebf056961f633b298053b41ad33c0e1b8de79d
John Gyselinck <johngyselinck@gmail.com>
2013-07-12 17:03:21 -0400

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buff = Buffer(fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString);
 });
 
 var port = process.env.PORT || 5000;
