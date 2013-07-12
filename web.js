a84d326c4fba182c32c98cc2244f22a17b455308
John Gyselinck <johngyselinck@gmail.com>
2013-07-12 17:07:29 -0400

    Corrected missing parentheses

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var buff = Buffer(fs.readFileSync(index.html);
+var buff = Buffer(fs.readFileSync(index.html));
 
 app.get('/', function(request, response) {
   response.send(buff.toString);
